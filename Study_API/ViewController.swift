876787a77c5fba559b5a2986b0275e9486e8c798
minaje0917 <k050918@naver.com>
2022-04-30 17:40:42 +0900

    Initial Commit

---
+++ Study_API/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Study_API
+//
+//  Created by 선민재 on 2022/04/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
