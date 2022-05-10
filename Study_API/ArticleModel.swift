11c61faa6ae2ef7661156f65e9d98029b348c4cd
minaje0917 <k050918@naver.com>
2022-05-10 23:18:39 +0900

    Part2

--- Study_API/ArticleModel.swift
+++ Study_API/ArticleModel.swift
@@ -16,7 +16,18 @@ class ArticleModel {
     var delegate:ArticleModelProtocol?
     
     func getArticles() {
-        
+        // 1. 먼저 요청할 url를 String으로 담아둔다.
+        let urlString = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c98744baeb8e4146b1007981eed8056d"
+        // 2. 해당 스트링으로 URL인스턴스를 생성해주세요.
+        let url = URL(string: urlString)
+        // 3. 해당 url이 만약에 nil값이라면 이곳에서 중지한다.
+        guard url != nil else {
+            print("Couldn't create url object")
+            return
+        }
+        // 4. URLSession을 만들어준다.
+        let session = URLSession.shared
+        // 5. 
     }
     delegate?.articleRetrieved(article: [Article])
 }
